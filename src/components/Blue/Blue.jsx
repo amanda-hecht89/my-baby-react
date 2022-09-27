0e6f6bfdcebe4d3ca00b0613258d4cb2cacc4060
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-27 12:15:34 -0600

    css fun!

--- src/components/Blue/Blue.jsx
+++ src/components/Blue/Blue.jsx
@@ -4,12 +4,12 @@ import styles from './Blue.css';
 export default function Blue() {
   return (
     <div className={styles.Blue}>
-      <h1>Blue</h1>
       <nav>
         <Link to="baby blue">BabyBlue</Link>
         <Link to="dark blue">DarkBlue</Link>
         <Link to="blue green">BlueGreen</Link>
       </nav>
+      <h1>Blue</h1>
       <Outlet />
     </div>
   );
