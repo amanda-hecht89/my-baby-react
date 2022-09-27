afe047590cf2d1bf411fc8c4a7c93ae741c67425
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-26 18:51:51 -0600

    added folders

---
+++ src/components/Blue/BabyBlue.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BabyBlue() {
+  return (
+    <div>BabyBlue</div>
+  );
+}
---
+++ src/components/Blue/BlueGreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function BlueGreen() {
+  return (
+    <div>BlueGreen</div>
+  );
+}
