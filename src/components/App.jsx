2d56d467130dfcf0f438b56df1b8c0e6002ba9b0
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-09-27 10:42:38 -0600

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,24 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
+import Colors from './Colors/Colors.jsx';
+import Orange from './Orange/Orange.jsx';
+import Blue from './Blue/Blue.jsx';
+import Layout from './Page/Layout.jsx';
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        <Route element={<Layout />} >
+          <Route index element={<Colors />} />
+          <Route path="orange" element={<Orange />} />
+          <Route path="blue" element={<Blue />} />
+        </Route>
+
+
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
